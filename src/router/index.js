2f42cf215542c2bd9cb5e6600bdaecd719e21a40
Wesley Coelho <wesleyribeiro123@gmail.com>
2021-08-23 14:22:07 -0300

    criação das rotas para as demais filiais

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,45 @@ const routes = [
     component: () => import('../views/pages/010101.vue')
   },
   {
+    path: '/010103',
+    name: 'Cicopal Uberlândia',
+    component: () => import('../views/pages/010103.vue')
+  },
+  {
     path: '/010105',
     name: 'Cicopal Marabá',
     component: () => import('../views/pages/010105.vue')
-  }
+  },
+  {
+    path: '/010201',
+    name: 'Snacks Matriz',
+    component: () => import('../views/pages/010201.vue')
+  },
+  {
+    path: '/010401',
+    name: 'Carper',
+    component: () => import('../views/pages/010401.vue')
+  },
+  {
+    path: '/060101',
+    name: 'MA Distribuição',
+    component: () => import('../views/pages/060101.vue')
+  },
+  {
+    path: '/060102',
+    name: 'MA Bacabal',
+    component: () => import('../views/pages/060102.vue')
+  },
+  {
+    path: '/070101',
+    name: 'DF Distribuição',
+    component: () => import('../views/pages/070101.vue')
+  },
+  {
+    path: '/080101',
+    name: 'Comercial TO',
+    component: () => import('../views/pages/080101.vue')
+  },
 ]
 
 const router = createRouter({
