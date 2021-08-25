408ab1dfaebbe90b1de6e1c2154bfb09a46c4975
Wesley Coelho <wesleyribeiro123@gmail.com>
2021-08-25 17:58:43 -0300

    ajustes do erro 404

---
+++ src/layout/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <q-layout view="hhh lpR lFf" container class="shadow-2 rounded-borders window-height window-width">
+      <NavBar />
+      <Drawer />
+      <q-page-container>
+        <q-page padding>
+          <router-view />
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import Drawer from '../components/Drawer.vue'
+export default {
+  components: {
+    NavBar,
+    Drawer
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,56 +2,61 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/index.vue')
+    path: "/",
+    component: () => import("../layout/Main.vue"),
+    children: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/index.vue')
+    },
+    {
+      path: '/010101',
+      name: 'Cicopal Matriz',
+      component: () => import('../views/pages/010101.vue')
+    },
+    {
+      path: '/010103',
+      name: 'Cicopal Uberlândia',
+      component: () => import('../views/pages/010103.vue')
+    },
+    {
+      path: '/010105',
+      name: 'Cicopal Marabá',
+      component: () => import('../views/pages/010105.vue')
+    },
+    {
+      path: '/010201',
+      name: 'Snacks Matriz',
+      component: () => import('../views/pages/010201.vue')
+    },
+    {
+      path: '/010401',
+      name: 'Carper',
+      component: () => import('../views/pages/010401.vue')
+    },
+    {
+      path: '/060101',
+      name: 'MA Distribuição',
+      component: () => import('../views/pages/060101.vue')
+    },
+    {
+      path: '/060102',
+      name: 'MA Bacabal',
+      component: () => import('../views/pages/060102.vue')
+    },
+    {
+      path: '/070101',
+      name: 'DF Distribuição',
+      component: () => import('../views/pages/070101.vue')
+    },
+    {
+      path: '/080101',
+      name: 'Comercial TO',
+      component: () => import('../views/pages/080101.vue')
+    },
+    ]
   },
-  {
-    path: '/010101',
-    name: 'Cicopal Matriz',
-    component: () => import('../views/pages/010101.vue')
-  },
-  {
-    path: '/010103',
-    name: 'Cicopal Uberlândia',
-    component: () => import('../views/pages/010103.vue')
-  },
-  {
-    path: '/010105',
-    name: 'Cicopal Marabá',
-    component: () => import('../views/pages/010105.vue')
-  },
-  {
-    path: '/010201',
-    name: 'Snacks Matriz',
-    component: () => import('../views/pages/010201.vue')
-  },
-  {
-    path: '/010401',
-    name: 'Carper',
-    component: () => import('../views/pages/010401.vue')
-  },
-  {
-    path: '/060101',
-    name: 'MA Distribuição',
-    component: () => import('../views/pages/060101.vue')
-  },
-  {
-    path: '/060102',
-    name: 'MA Bacabal',
-    component: () => import('../views/pages/060102.vue')
-  },
-  {
-    path: '/070101',
-    name: 'DF Distribuição',
-    component: () => import('../views/pages/070101.vue')
-  },
-  {
-    path: '/080101',
-    name: 'Comercial TO',
-    component: () => import('../views/pages/080101.vue')
-  },
-
 
   // Always leave this as last one,
   // but you can also remove it
