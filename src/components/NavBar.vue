be6cf724613bca514567b6ba3bf3abb1b20ba826
Wesley Coelho <wesleyribeiro123@gmail.com>
2021-08-25 14:44:16 -0300

    informando controle de versão do app

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,31 @@
 <template>
   <q-header elevated class="bg-primary">
     <q-toolbar>
-      <q-toolbar-title class="text-center">
-        Pedidos Importação - SAIB vs Protheus
+      <q-toolbar-title class="text-center col justify-center">
+        <div class="text-center">
+          Pedidos Importação - SAIB vs Protheus
+        </div>
+        <div class="text-subtitle2 text-center text-version">
+          v. {{ version }}
+        </div>
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
 </template>
+
+<script>
+import { version } from '../../package.json'
+export default {
+  data() {
+    return {
+      version: version
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .text-version {
+    color: #555675;
+  }
+</style>
